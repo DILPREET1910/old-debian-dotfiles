bdd168fe4164a022d7a1d7b56465f34d9ac1a966
DILPREET1910 <dsahney1910@gmail.com>
2023-10-06 15:00:42 +0530

    ejs synatx highlightig

---
+++ .config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,13 @@
+local plugins = {
+  -- wakatime plugins
+  {
+    "wakatime/vim-wakatime" , lazy = false
+  },
+
+  -- ejs syntax highlighting
+  {
+    'nikvdp/ejs-syntax', lazy = false
+  }
+}
+
+return plugins
